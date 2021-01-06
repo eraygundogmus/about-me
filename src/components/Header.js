448b2efcd955a932deda87862300b325ac32deb5
Eray Gündoğmuş <gundogmuseray@gmail.com>
2021-01-06 21:53:12 +0300

    First Commit! Component files created into components folder, README.md changed, add React- Router as Depencies.

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="navbar">
+            <header className="my-header"> 
+                    <h3 className="my-logo"> Front End Developer</h3>
+                <ul>
+                    <li> <a href="{Aboutme.js}">About Me</a></li>
+                    <li> <a href="{Aboutme.js}">Works</a> </li>
+                    <li> <a href="{Aboutme.js}">Comments</a> </li>
+                    <li> <a href="{Aboutme.js}">Contact</a></li>
+                </ul>
+            </header>            
+       </div>
+    )
+}
+
+export default Header
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div className="Hero__me">
+    
+       </div>
+    )
+}
+
+export default Hero
