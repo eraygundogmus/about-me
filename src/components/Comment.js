56018a8b20c8e1bd83e9753730d9b2a7d3eb6f42
mbugrayildiz <mbugrayildiz@outlook.com>
2021-01-07 18:47:03 +0300

    it now works, probably

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,50 +1,21 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import userEvent from "@testing-library/user-event";
+import React from "react"
+import { CommentContext } from "../context"
 
+const Comment = ({id, text, email, name}) => {
+    const ctx = React.useContext(CommentContext)
 
-class Comment extends Component {
-    static defaultProps = {
-        title : "İsim Soyisim",
-        email : "...@gmail.com",
-        thecomment : "Lorem ipsum dolor sim amet"
-
-    }
-
-
-    render() {
-
-        const {title, email, thecomment} = this.props;
-        return (
-
-            <div className= "col-md-8 mb-4">
-                <div className= "card">
-                    <div className = "card-header d-flex justify-content-between">
-                        <h5 className = "d-inline">{title}</h5>
-                            <i className = "far fa-trash-alt" style = {{cursor : "pointer"}}></i>
-                </div>
-                <div className="card-body">
-                    <p className="card-text"> {email}</p>
-                    <p className="card-text"> {thecomment}</p>
-                    </div>
-
-                    </div>
-                </div>
-
-        )
-    }
-
-
+    return <div className= "col-md-8 mb-4">
+        <div className= "card">
+            <div className = "card-header d-flex justify-content-between">
+                <h5 className = "d-inline">{name}</h5>
+                <i onClick={() => ctx.removeComment(id)} className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
+            </div>
+        </div>
+        <div className="card-body">
+            <p className="card-text"> {email}</p>
+            <p className="card-text"> {text}</p>
+        </div>
+    </div>
 }
-
-
-Comment.propTypes = {
-    name : PropTypes.string.isRequired,
-    email : PropTypes.string.isRequired,
-    thecomment : PropTypes.string.isRequired
-
-
-}
-
 
 export default Comment
